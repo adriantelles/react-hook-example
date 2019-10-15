fbc5cf35a5516bd57d9dee1515ac2813277b42ea
Webmaster0104 <awesomewebdev01@gmail.com>
2019-10-16 01:20:40 +1000

    useContext step 1

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
-import React, { useReducer , useState } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Index }  from "./pages"
+import { About }  from "./pages/about"
 
-// const reducer = (state, action)=>{
-//   switch(action.type){
-//     case 'increment':
-//         return state + 1;
-//     case 'decrement':
-//         return state - 1;
-//     default:
-//         return state;
-//   }
-// }
-
-const reducer = (state, action)=>{
-  switch(action.type){
-    case 'add-todo':
-        return {...state ,todos:[...state.todos, {text: action.text, completed: false}]}
-    case 'toogle-todo':
-            return {todos: state.todos.map((t,index)=> index === action.index ? {...t, completed: !t.completed} : t)}
-    default:
-        return state;
-  }
-}
-const App = ()=>{
-
-  const [{todos}, dispatch] = useReducer(reducer, { todos: []})
-  const [text, setText] = useState();
-  return(
-    <div>
+export default function AppRouter() {
+  return (
+    <Router>
       <div>
-          <form onSubmit = {e=>{
-            e.preventDefault();
-            dispatch({type: "add-todo", text})
-            setText("");
-          }}>
-            <input value = {text} onChange ={e=>setText(e.target.value)} />
-          </form>
-          {todos.map((t,idx)=>(
-            <div key = {t.text} onClick = {()=>dispatch({type:'toogle-todo',index:idx})} style = {{textDecoration: t.completed ? 'line-through' : ''}}>
-            {t.text}</div>
-          ))}
-      </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <div>
-        {/* <div>count: {count}</div>
-        <button onClick= {()=>dispatch({type:'increment'})}>increment</button>
-        <button onClick= {()=>dispatch({type:'decrement'})}>decrement</button> */}
+        <Route path="/" exact component={Index}></Route>
+        <Route path="/about"  component={About}></Route>
       </div>
-    </div>
+    </Router>
   )
 }
-
-export default App;
